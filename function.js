9f9d9423e145c2a1d0c61a83beaf700c17b8bd48
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-07-29 12:04:06 +0600

    added

--- function.js
+++ function.js
@@ -3,4 +3,7 @@ function add(num1, num2){
     console.log(num1, num2, result);
     return result 
 }
-const sum = add()
+const sum = add(3, 5)
+const a = 'Razib'
+const name = `my name is ${a}`
+console.log(name);
